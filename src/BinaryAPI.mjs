0fe55309f827cea0fad4c7a4c4d86c6395d44eee
Amin Marashi <marashi.s.amin@gmail.com>
2019-06-20 14:41:58 +0800

    Add initial modules

---
+++ src/BinaryAPI.mjs
@@ -0,0 +1,9 @@
+import BinaryAPICalls from './BinaryAPICalls';
+
+export default class BinaryAPI extends BinaryAPICalls {
+    constructor({connection, endpoint = 'ws.binaryws.com', appId = 1, lang = 'EN'}) {
+        this.connection = connection ?
+            connection :
+            WebSocket(`wss:\/\/${endpoint}/v3/websocket?l=${lang}&app_id=${appId}`)
+    }
+}

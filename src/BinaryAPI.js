f512913284d7198c036e5be2ad1a0d5027c827c8
binary-hamedanchi <mohammad@binary.com>
2019-06-21 09:17:06 +0800

    Basic events and methods

--- src/BinaryAPI.js
+++ src/BinaryAPI.js
@@ -1,10 +1,61 @@
-import BinaryAPICalls from './BinaryAPICalls.js';
+import BinaryAPICalls from './BinaryAPICalls';
+import CustomPromise  from './CustomPromise';
 
 export default class BinaryAPI extends BinaryAPICalls {
-    constructor({connection, endpoint = 'ws.binaryws.com', appId = 1, lang = 'EN'} = {}) {
-        super()
-        this.connection = connection ?
-            connection :
-            new WebSocket(`wss:\/\/${endpoint}/v3/websocket?l=${lang}&app_id=${appId}`)
+    constructor({ connection, endpoint = 'red.binaryws.com', appId = 1, lang = 'EN' } = {}) {
+        super();
+
+        if (connection) {
+            this.connection = connection;
+        } else {
+            this.connectionArgs = { endpoint, appId, lang };
+            this.connect();
+        }
+
+        this.connection.onopen    = this.onOpen.bind(this);
+        this.connection.onclose   = this.onClose.bind(this);
+        this.connection.onmessage = this.onMessage.bind(this);
+
+        this.connected = new CustomPromise();
+        this.pendingRequests = {};
+    }
+
+    connect() {
+        const { endpoint, lang, appId } = this.connectionArgs;
+        this.connection = new WebSocket(`wss:\/\/${endpoint}/websockets/v3?l=${lang}&app_id=${appId}`);
+    }
+
+    onOpen() {
+        if (this.connection.readyState === 1) {
+            this.connected.resolve();
+        } else {
+            setTimeout(this.onOpen, 50);
+        }
+    }
+
+    onClose() {
+        if (this.connectionArgs) {
+            this.connect();
+        }
+    }
+
+    onMessage(msg) {
+        if (!msg.data) {
+            throw new Error('Something went wrong while receiving the response from API.');
+        }
+
+        const response = JSON.parse(msg.data);
+        this.pendingRequests[response.req_id].resolve(response);
+        delete this.pendingRequests[response.req_id];
+    }
+
+    async send(request) {
+        await this.connected;
+
+        this.connection.send(JSON.stringify(request));
+
+        const pending = new CustomPromise();
+        this.pendingRequests[request.req_id] = pending;
+        return pending;
     }
 }

d47327fb6ed68add096b35a0f63c1d3b42c07e0a
binary-hamedanchi <mohammad@binary.com>
2019-06-21 09:17:47 +0800

    Add send test

--- src/__tests__/api.js
+++ src/__tests__/api.js
@@ -1,6 +1,11 @@
-import BinaryAPI from '../BinaryAPI.js'
+import ws        from 'ws';
+import BinaryAPI from '../BinaryAPI'
 
-test('Constructing BinaryAPI', () => {
-    const api = new BinaryAPI();
-    expect(api).toBeInstanceOf(BinaryAPI)
-})
+test('Constructing BinaryAPI', async() => {
+    const connection = new ws('wss://blue.binaryws.com/websockets/v3?app_id=1&l=EN');
+    const api = new BinaryAPI({ connection });
+    expect(api).toBeInstanceOf(BinaryAPI);
+
+    const response = await api.ping();
+    expect(response.ping).toEqual('pong');
+});

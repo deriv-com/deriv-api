9d78e91b3b800dad7c37e826e58313213cdb4580
Amin Marashi <marashi.s.amin@gmail.com>
2019-07-11 15:04:57 +0800

    Update DerivAPICalls to accept non-object params

--- src/Streams/TickStream.js
+++ src/Streams/TickStream.js
@@ -29,7 +29,7 @@ export default class TickStream extends Stream {
     }
 
     async init() {
-        const activeSymbols = (await this.api.cache.activeSymbols({ active_symbols: 'brief' })).active_symbols;
+        const activeSymbols = (await this.api.cache.activeSymbols('brief')).active_symbols;
         this._data.pip      = activeSymbols.find(s => s.symbol === this.symbol).pip;
         const tickStream    = this.api.subscribe(requestParams(this.symbol, this.range));
 

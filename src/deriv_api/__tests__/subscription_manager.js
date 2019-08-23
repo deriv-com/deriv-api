355f0d921210832885f7c9b31bc05a5608b6029c
Amin Marashi <marashi.s.amin@gmail.com>
2019-08-23 12:09:53 +0800

    Add test for removing subscribers

--- src/deriv_api/__tests__/subscription_manager.js
+++ src/deriv_api/__tests__/subscription_manager.js
@@ -55,6 +55,38 @@ test('Subscribe then forget then subscribe again', async () => {
     const second_response = await source2.pipe(first()).toPromise();
 
     expect(second_response.subscription.id).not.toBe(first_response.subscription.id);
+
+    await api.forget(second_response.subscription.id);
+});
+
+describe('Check sources', () => {
+    let source_R_100_1;
+    let source_R_100_2;
+    let source_R_25;
+
+    test('Subscribe twice to the same things returns the same sources', () => {
+        source_R_100_1 = api.subscribe({ ticks: 'R_100' });
+        source_R_100_2 = api.subscribe({ ticks: 'R_100' });
+
+        expect(source_R_100_1).toBe(source_R_100_2);
+    });
+
+    test('Subscribe twice to the different things returns different sources', () => {
+        source_R_25 = api.subscribe({ ticks: 'R_25' });
+
+        expect(source_R_100_1).not.toBe(source_R_25);
+    });
+
+    test('Unsubscrbe from one source and subscribe again, a new source is returned', () => {
+        const old_source = api.subscribe({ ticks: 'R_50' });
+
+        // Subscribe and unsubscribe immediately
+        old_source.subscribe(() => {}).unsubscribe();
+
+        const new_source = api.subscribe({ ticks: 'R_50' });
+
+        expect(new_source).not.toBe(source_R_25);
+    });
 });
 
 beforeAll(() => {

cbcc9a2f2496ab488d0c686c530e895a0fbd9127
Bala <bala.krishna@regentmarkets.com>
2020-09-29 18:54:55 +0800

    revert webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,54 @@
-const path = require("path");
-const webpack = require("webpack");
+const path    = require('path');
+const webpack = require('webpack');
 
 module.exports = [
-  {
-    mode: "development",
-    entry: "./src/deriv_api/DerivAPIBasic.js",
-    output: {
-      filename: "DerivAPIBasic.js",
-      globalObject: "typeof self !== 'undefined' ? self : this", // Because webpack sucks
-      library: "DerivAPIBasic",
-      libraryExport: "default",
-      libraryTarget: "umd",
-      path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
+    {
+        mode  : 'production',
+        entry : './src/deriv_api/DerivAPIBasic.js',
+        output: {
+            filename     : 'DerivAPIBasic.js',
+            globalObject : "typeof self !== 'undefined' ? self : this", // Because webpack sucks
+            library      : 'DerivAPIBasic',
+            libraryExport: 'default',
+            libraryTarget: 'umd',
+            path         : path.resolve(__dirname, 'dist'),
         },
-      ],
-    },
-  },
-  {
-    plugins: [new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
-    mode: "development",
-    entry: "./src/DerivAPI.js",
-    output: {
-      filename: "DerivAPI.js",
-      globalObject: "typeof self !== 'undefined' ? self : this", // Because webpack sucks
-      library: "DerivAPI",
-      libraryExport: "default",
-      libraryTarget: "umd",
-      path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
+        module: {
+            rules : [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    }
+                },
+            ]
         },
-      ],
     },
-  },
+    {
+        plugins: [
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        ],
+        mode  : 'production',
+        entry : './src/DerivAPI.js',
+        output: {
+            filename     : 'DerivAPI.js',
+            globalObject : "typeof self !== 'undefined' ? self : this", // Because webpack sucks
+            library      : 'DerivAPI',
+            libraryExport: 'default',
+            libraryTarget: 'umd',
+            path         : path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules : [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    }
+                },
+            ]
+        },
+    },
 ];

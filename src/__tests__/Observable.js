87025d6ca739aceaff675862f7c3bbe428c087d8
chylli-binary <chylli@binary.com>
2021-08-13 01:47:45 +0000

    fix another error

--- src/__tests__/Observable.js
+++ src/__tests__/Observable.js
@@ -5,16 +5,17 @@ import {
     first,
     toArray,
 }                       from 'rxjs/operators';
-import { interval, Subject, Observable }     from 'rxjs';
+import {
+    interval, Subject, Observable, firstValueFrom,
+}     from 'rxjs';
 
 test('Pure observable', async () => {
     const source = interval(100);
-    const result = await source
+    const result = await firstValueFrom(source
         .pipe(
             take(2),
             toArray(),
-        )
-        .toPromise();
+        ));
     expect(result).toEqual([0, 1]);
 });
 
@@ -24,12 +25,11 @@ test('Observable (with observer.next)', async () => {
         observer.next(1);
         observer.complete();
     });
-    const array        = await every_second
+    const array        = await firstValueFrom(every_second
         .pipe(
             take(2),
             toArray(),
-        )
-        .toPromise();
+        ));
 
     expect(array).toEqual([0, 1]);
 });
@@ -40,12 +40,11 @@ test('Subject (with source.next)', async () => {
     let counter = 0;
     setInterval(() => every_second.next(counter++), 100);
 
-    const array = await every_second
+    const array = await firstValueFrom(every_second
         .pipe(
             take(2),
             toArray(),
-        )
-        .toPromise();
+        ));
 
     expect(array).toEqual([0, 1]);
 });
@@ -55,7 +54,7 @@ test('Observable throwing error', async () => {
 
     error_source.pipe(catchError((e) => expect(e).toBeInstanceOf(Error)));
 
-    const promise = error_source.toPromise();
+    const promise = firstValueFrom(error_source);
 
     error_source.error(Error('Oops, err...'));
 
@@ -70,10 +69,10 @@ test('Subject with skip', async () => {
 
     const with_skip = source.pipe(skip(1));
 
-    const items = await with_skip.pipe(take(2), toArray()).toPromise();
+    const items = await firstValueFrom(with_skip.pipe(take(2), toArray()));
 
     // source is a Subject which by default is a hot observable
-    const items_after_await = await source.pipe(first()).toPromise();
+    const items_after_await = await firstValueFrom(source.pipe(first()));
 
     expect(items).toEqual([1, 2]);
 

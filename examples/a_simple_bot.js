0f6ecbfcafcf26ae0a7e7dd1be9edf14656ef78c
Ramin <ramin@re-work.dev>
2021-09-15 00:38:02 +0430

    - Fix a_simple_bot_websocket problem

--- examples/a_simple_bot.js
+++ examples/a_simple_bot.js
@@ -1,17 +1,25 @@
-global.WebSocket = require('ws');
+const WS = require('ws');
 const { find } = require('rxjs/operators');
 const DerivAPI = require('../dist/DerivAPI');
 
 const token = process.env.DERIV_TOKEN;
 const app_id = process.env.APP_ID || 1234;
 const expected_payout = process.env.EXPECTED_PAYOUT || 19;
+const lang       = 'EN';
+const brand      = '';
 
 if (!token) {
     console.error('DERIV_TOKEN environment variable is not set');
     process.exit(1);
 }
 
-const api = new DerivAPI({ app_id });
+/**
+ * If we want to use the Deriv-API library in the Nodejs environment we can create ready to use connection
+ * and pass it to the `DerivAPI` constructor.
+ */
+const connection = new WS(`wss://frontend.binaryws.com/websockets/v3?app_id=${app_id}&l=${lang}&brand=${brand}`);
+
+const api = new DerivAPI({ app_id, connection });
 
 async function main() {
     try {

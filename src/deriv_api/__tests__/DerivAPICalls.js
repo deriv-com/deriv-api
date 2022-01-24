931c16094236ac44f97b0b570060a5d5b446aa75
sasikala-binary <sasikala@deriv.com>
2022-01-24 16:26:37 +0000

    test code coverage

--- src/deriv_api/__tests__/DerivAPICalls.js
+++ src/deriv_api/__tests__/DerivAPICalls.js
@@ -8,6 +8,10 @@ test('Constructing DerivAPICalls', async () => {
     const crypt_config_request = { crypto_config: 1 };
     const response             = await api.cryptoConfig(crypt_config_request);
     expect(response).toEqual(crypt_config_request);
+
+    const website_status_request = { website_status: 1 };
+    const response1              = await api.websiteStatus(website_status_request);
+    expect(response1).toEqual(website_status_request);
 });
 
 beforeAll(() => {

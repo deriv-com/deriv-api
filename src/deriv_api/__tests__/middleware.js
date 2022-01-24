cecdbd55dc71a2a56db53ace6ae8c765469e233c
sasikala-binary <sasikala@deriv.com>
2022-01-24 11:11:23 +0000

    test

--- src/deriv_api/__tests__/middleware.js
+++ src/deriv_api/__tests__/middleware.js
@@ -17,6 +17,12 @@ test('Expect middleware functions to be called on send', async () => {
     expect(middleware.sendWillBeCalled).toBeCalledWith({ args: [request] });
     expect(middleware.sendIsCalled)
         .toBeCalledWith({ response_promise: new Promise(() => {}), args: [request] });
+
+    const crypt_config_request = { crypto_config: 1 };
+    api.send(crypt_config_request);
+    expect(middleware.sendWillBeCalled).toBeCalledWith({ args: [crypt_config_request] });
+    expect(middleware.sendIsCalled)
+        .toBeCalledWith({ response_promise: new Promise(() => {}), args: [crypt_config_request] });
 });
 
 test('Expect sendIsCalled to be called if sendWillBeCalled returns undefined', async () => {

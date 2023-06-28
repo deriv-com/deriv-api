320984ef96b66065aea88e573b8280019f29d1d0
yashim-deriv <yashim@regentmarkets.com>
2023-06-28 13:25:05 +0800

    fix: updated bot example

--- examples/a_simple_bot.js
+++ examples/a_simple_bot.js
@@ -1,13 +1,13 @@
-global.WebSocket = require('ws');
-const { find } = require('rxjs/operators');
-const DerivAPI = require('../dist/DerivAPI');
+global.WebSocket = require("ws");
+const { find } = require("rxjs/operators");
+const DerivAPI = require("../dist/DerivAPI");
 
 const token = process.env.DERIV_TOKEN;
 const app_id = process.env.APP_ID || 1234;
 const expected_payout = process.env.EXPECTED_PAYOUT || 19;
 
 if (!token) {
-    console.error('DERIV_TOKEN environment variable is not set');
+    console.error("DERIV_TOKEN environment variable is not set");
     process.exit(1);
 }
 
@@ -26,42 +26,45 @@ async function main() {
         });
 
         const contract = await api.contract({
-            contract_type: 'CALL',
+            contract_type: "CALL",
             currency,
             amount: 10,
-            duration: 5,
-            duration_unit: 't',
-            symbol: 'frxUSDJPY',
-            basis: 'stake',
+            duration: 10,
+            duration_unit: "m",
+            symbol: "frxUSDJPY",
+            basis: "stake",
         });
 
         contract.onUpdate(({ status, payout, bid_price }) => {
             switch (status) {
-                case 'proposal':
-                    return console.log(
-                        `Current payout: ${payout.currency} ${payout.display}`);
-                case 'open':
-                    return console.log(
-                        `Current bid price: ${bid_price.currency} ${bid_price.display}`);
+                case "proposal":
+                    return console.log(`Current payout: ${payout.currency} ${payout.display}`);
+                case "open":
+                    return console.log(`Current bid price: ${bid_price.currency} ${bid_price.display}`);
                 default:
                     break;
-            };
+            }
         });
 
         // Wait until payout is greater than USD 19
-        await contract.onUpdate().pipe(find(({ payout }) => payout.value >= expected_payout)).toPromise();
+        await contract
+            .onUpdate()
+            .pipe(find(({ payout }) => payout.value >= expected_payout))
+            .toPromise();
 
         const buy = await contract.buy();
 
         console.log(`Buy price is: ${buy.price.currency} ${buy.price.display}`);
 
         // Wait until the contract is sold
-        await contract.onUpdate().pipe(find(({ is_sold }) => is_sold)).toPromise();
+        await contract
+            .onUpdate()
+            .pipe(find(({ is_sold }) => is_sold))
+            .toPromise();
 
         const { profit, status } = contract;
 
         console.log(`You ${status}: ${profit.currency} ${profit.display}`);
-
     } catch (err) {
         console.error(err);
     } finally {
